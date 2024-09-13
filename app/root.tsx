e2c469f6589a7ef624cbc44b57c7a00e21794d9e
FrazierMark <m.mark.frazier@gmail.com>
2024-09-13 13:39:19 -0700

    Fix warnings in browser

--- app/root.tsx
+++ app/root.tsx
@@ -51,7 +51,6 @@ import { makeTimings, time } from './utils/timing.server.ts'
 import { getToast } from './utils/toast.server.ts'
 import { useOptionalUser, useUser } from './utils/user.ts'
 
-
 export const links: LinksFunction = () => {
 	return [
 		// Preload svg sprite as a resource to avoid render blocking
@@ -176,6 +175,7 @@ function Document({
 				<Links />
 			</head>
 			<body className="bg-background text-foreground">
+				<CanvasScene theme={theme} />
 				{children}
 				<script
 					nonce={nonce}
@@ -186,7 +186,6 @@ function Document({
 				<ScrollRestoration nonce={nonce} />
 				<Scripts nonce={nonce} />
 			</body>
-			<CanvasScene theme={theme} />
 		</html>
 	)
 }
@@ -210,7 +209,7 @@ function App() {
 			env={data.ENV}
 		>
 			<div className="flex h-screen flex-col justify-between">
-				<header className="container py-6 border-b border-gray-200 dark:border-gray-700 pb-2">
+				<header className="container border-b border-gray-200 py-6 pb-2 dark:border-gray-700">
 					<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
 						<Logo />
 						<div className="ml-auto hidden max-w-sm flex-1 sm:block">
@@ -233,7 +232,6 @@ function App() {
 				<div className="flex-1">
 					<Outlet />
 				</div>
-
 			</div>
 			<EpicToaster closeButton position="top-center" theme={theme} />
 			<EpicProgress />
